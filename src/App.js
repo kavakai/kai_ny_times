8fb0d099ea252c8b1e97e8cc2358158c9c9dd36d
Kai Kuller <kavakai@gmail.com>
2022-04-18 16:36:19 -0600

    Add css to main page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect} from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { getArticles } from "./apiCalls"
 import AbstractSection from "./Components/AbstractSection"
-import './css_reset.css';
+import "./css_reset.css";
+import "./App.css"
 
 function App() {
 
--- src/Components/Abstract.js
+++ src/Components/Abstract.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Abstract.css"
 
 function Abstract({id, title, image, alt, date}) {
 
   return (
-    <div>
+    <div className="article_card">
       <img src={image} alt={alt}/>
       <h2>{title}</h2>
       <p>{date}</p>
